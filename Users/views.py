8dc8828eeaf862c9338bb4762fa3ed14966afb4d
gvLeonardo <gv_leonardo@outlook.com>
2020-10-28 20:45:08 -0700

    base para comenzar agregada

--- Users/views.py
+++ Users/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def landing_view(request):
+    """Landing view"""
+    return render(request, 'landing.html')
+    
+def hello_world(request):
+    return HttpResponse('<h1>Hola mundo jeje</h1>')
