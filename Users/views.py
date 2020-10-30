7bc61365697a4c663483dd8239c8279270468ee4
gvLeonardo <gv_leonardo@outlook.com>
2020-10-29 20:20:52 -0700

    landing, login y register inicio

--- Users/views.py
+++ Users/views.py
@@ -6,6 +6,18 @@ from django.shortcuts import render
 def landing_view(request):
     """Landing view"""
     return render(request, 'landing.html')
+
+
+def login_view(request):
+    """Login view"""
+    return render(request,'users/login.html')
+
+
+def register_view(request):
+    """Register view"""
+    return render(request, 'users/register.html')
+
     
 def hello_world(request):
+    """Test view"""
     return HttpResponse('<h1>Hola mundo jeje</h1>')
