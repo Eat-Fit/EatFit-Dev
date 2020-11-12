d558fe8279b227a0da9cfa1c71cf91d6fde31d3a
gvLeonardo <gv_leonardo@outlook.com>
2020-11-11 19:40:52 -0700

    pantalla home terminada

--- services/views.py
+++ services/views.py
@@ -2,8 +2,12 @@
 
 from django.shortcuts import render
 
+# MODELS
+from Users.models import Nutriologist
+
 # Create your views here.
 
 def home_view (request):
     """home view"""
-    return render(request, 'services/home_page.html')
+    nutri = Nutriologist.objects.all().filter(active=True).order_by('?')
+    return render(request, 'services/home_page.html', {'nutris': nutri})
